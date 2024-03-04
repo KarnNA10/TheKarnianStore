b37375d9e1715ee30091575b42f01d81d9224698
KarnNA10 <jackyel9911@gmail.com>
2024-03-04 08:03:51 -0600

    New!

---
+++ ff.js
@@ -0,0 +1,23 @@
+function amp(imgDir, type, price, descr){
+    const topCont = document.querySelector('.topCont')
+    topCont.style.display = 'block'
+
+    const imag = document.createElement('img')
+    imag.src  = "static/img/prod/"+imgDir
+    document.querySelector('.e1').appendChild(imag)
+    imag.setAttribute("class","prodImgAmp")
+
+    document.getElementById("type").innerHTML=type;
+    document.getElementById("descrip").innerHTML=descr;
+    document.getElementById("prec").innerHTML="$"+price;
+}
+function cerrar() {
+    // topCont.style.background = '#00ff00'
+    const topCont = document.querySelector('.topCont');
+    topCont.style.display = 'none'; 
+    document.getElementById("type").innerHTML = '';
+    document.getElementById("descrip").innerHTML = '';
+    document.getElementById("prec").innerHTML = '';
+    var imag = document.querySelector('.prodImgAmp')
+    imag.remove()
+}
